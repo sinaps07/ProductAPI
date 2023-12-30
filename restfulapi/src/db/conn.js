60f6ae05ac31d0dbc7b07b8d466845a333456923
sinaps07 <sinaps3547@gmail.com>
2023-12-30 15:21:46 +0530

    added dotenv files

--- restfulapi/src/db/conn.js
+++ restfulapi/src/db/conn.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Product-api").then(()=>{
+const dotenv = require('dotenv');
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("connection is succesful");
 }).catch((err)=>{
 console.log("No connection");
