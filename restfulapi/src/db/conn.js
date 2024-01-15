8417aa85df2561ed22cc7151611335abf1f36a3e
sinaps07 <sinaps3547@gmail.com>
2024-01-15 15:02:34 +0530

    added files into gitignore

--- restfulapi/src/db/conn.js
+++ restfulapi/src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({path: "./vars/.env"});
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("connection is succesful");
 }).catch((err)=>{
